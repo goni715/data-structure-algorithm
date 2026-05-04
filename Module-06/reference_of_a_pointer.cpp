9f023429bde174690f9a59fd7437553fd1f6d426
goni715 <halesgoni715149123@gmail.com>
2026-05-04 22:11:26 +0600

    reference of a pointer

---
+++ Module-06/reference_of_a_pointer.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun1(int *p){
+    p = NULL;
+}
+void fun2(int * &p){ //reference of a pointer
+    p = NULL;
+}
+
+int main(){
+    int val = 10;
+    int *ptr = &val;
+    fun2(ptr);
+    cout << ptr << endl; //0
+    return 0;
+}
